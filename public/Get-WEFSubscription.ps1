bd69f51769112cb9d5d081d1c86dcdeb46f92232
Bayliss <cbaylissmk2@gmail.com>
2019-08-06 15:20:43 +0100

    reformatted allowing for multiple subs

--- public/Get-WEFSubscription.ps1
+++ public/Get-WEFSubscription.ps1
@@ -2,7 +2,7 @@ function Get-WEFSubscription {
     [CmdletBinding()]
     param (
         [string]$WECServer,
-        [string]$Subscription,
+        [string[]]$Subscription,
         [switch]$List,
 
         [ValidateSet('XML','Object')]
@@ -14,62 +14,61 @@ function Get-WEFSubscription {
     }
     
     process {
-        # create cmd
-        $cmd = "wecutil"
+        if ($PSBoundParameters.List) {
+            $cmd = "wecutil es"
+            $outList = Invoke-Command -ComputerName $WECServer -ScriptBlock { Invoke-Expression $args[0] } -ArgumentList $cmd
 
-        if ($PSBoundParameters.Subscription) {
-            # if subscription specified return config
-            $cmd += " gs $Subscription /f:XML"
-        } elseif ($PSBoundParameters.List) {
-            # if list, enum subscriptions
-            $cmd += " es"
-        } else {
-            Write-Error "Please specify subscription ID or -List"
-        }
-        # if no wec server prompted, localhost
-
-        if ($PSBoundParameters.WECServer) {
-            $res = Invoke-Command -ComputerName $WECServer -ScriptBlock { Invoke-Expression $args[0] } -ArgumentList $cmd
-        } else {
-            $res = Invoke-Expression $cmd
-        }
-
-        if ($PSBoundParameters.Format -eq 'XML') {
-            $out = $res
-        } else { 
-            [xml]$res = $res
-            $out = [PSCustomObject]@{
-                SubscriptionId                  = $res.Subscription.SubscriptionId
-                SubscriptionType                = $res.Subscription.SubscriptionType           
-                Description                     = $res.Subscription.Description
-                Enabled                         = $res.Subscription.Enabled
-                ConfigurationMode               = $res.Subscription.ConfigurationMode
-                Delivery                        = $null
-                Query                           = $res.Subscription.Query.'#cdata-section'
-                ReadExistingEvents              = $res.Subscription.ReadExistingEvents
-                TransportName                   = $res.Subscription.TransportName
-                ContentFormat                   = $res.Subscription.ContentFormat
-                Locale                          = $res.Subscription.Locale
-                LogFile                         = $res.Subscription.LogFile
-                AllowedSourceNonDomainComputers = $res.Subscription.AllowedSourceNonDomainComputers
-                AllowedSourceDomainComputers    = $res.Subscription.AllowedSourceDomainComputers
-            } 
-
-            $deliveryObj = @()
-            if ($res.Subscription.Delivery.Mode -eq 'Push') {
-                $deliveryObj = [PSCustomObject]@{
-                    Mode = $res.Subscription.Delivery.Mode
-                    Batching = [PSCustomObject]@{
-                        MaxItems = $res.Subscription.Delivery.Batching.MaxItems
-                        MaxLatencyTime = $res.Subscription.Delivery.Batching.MaxLatencyTime
-                    }
-                    PushSettings = [PSCustomObject]@{
-                        HeartBeatInterval = $res.Subscription.Delivery.PushSettings.Heartbeat.Interval
-                    }
+            $out = $outList
+        } elseif ($PSBoundParameters.Subscription) {
+            $res = @()
+            foreach ($sub in $Subscription) {
+                $cmd = "wecutil gs $sub /f:XML"
+                # if no wec server prompted, localhost
+                if ($PSBoundParameters.WECServer) {
+                    $res += Invoke-Command -ComputerName $WECServer -ScriptBlock { Invoke-Expression $args[0] } -ArgumentList $cmd
+                } else {
+                    $res += Invoke-Expression $cmd
                 }
             }
             
-            $out.Delivery = $deliveryObj
+            if ($PSBoundParameters.Format -eq 'XML') {
+                $out = $res
+            } else { 
+                foreach ($r in [xml]$res) {
+                    $out = [PSCustomObject]@{
+                        SubscriptionId                  = $r.Subscription.SubscriptionId
+                        SubscriptionType                = $r.Subscription.SubscriptionType           
+                        Description                     = $r.Subscription.Description
+                        Enabled                         = $r.Subscription.Enabled
+                        ConfigurationMode               = $r.Subscription.ConfigurationMode
+                        Delivery                        = $null
+                        Query                           = $r.Subscription.Query.'#cdata-section'
+                        ReadExistingEvents              = $r.Subscription.ReadExistingEvents
+                        TransportName                   = $r.Subscription.TransportName
+                        ContentFormat                   = $r.Subscription.ContentFormat
+                        Locale                          = $r.Subscription.Locale
+                        LogFile                         = $r.Subscription.LogFile
+                        AllowedSourceNonDomainComputers = $r.Subscription.AllowedSourceNonDomainComputers
+                        AllowedSourceDomainComputers    = $r.Subscription.AllowedSourceDomainComputers
+                    } 
+
+                    $deliveryObj = @()
+                    if ($r.Subscription.Delivery.Mode -eq 'Push') {
+                        $deliveryObj = [PSCustomObject]@{
+                            Mode = $r.Subscription.Delivery.Mode
+                            Batching = [PSCustomObject]@{
+                                MaxItems = $r.Subscription.Delivery.Batching.MaxItems
+                                MaxLatencyTime = $r.Subscription.Delivery.Batching.MaxLatencyTime
+                            }
+                            PushSettings = [PSCustomObject]@{
+                                HeartBeatInterval = $r.Subscription.Delivery.PushSettings.Heartbeat.Interval
+                            }
+                        }
+                    }
+                    
+                    $out.Delivery = $deliveryObj
+                }
+            }
         }
     }
     
