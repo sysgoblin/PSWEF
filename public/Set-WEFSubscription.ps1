7d8006c8c10358e8872c8df87800820d1a0c9077
Bayliss <cbaylissmk2@gmail.com>
2019-09-09 07:52:02 +0100

    basic function created

--- public/Set-WEFSubscription.ps1
+++ public/Set-WEFSubscription.ps1
@@ -2,19 +2,31 @@ function Set-WEFSubscription {
     [CmdletBinding()]
     param (
         [string]$WECServer,
-        [string]$Subscription
+        [string]$Subscription,
+        [ValidateSet('True', 'False')]
+        [string]$Retention,
+        [ValidateSet('True', 'False')]
+        [string]$AutoBackup,
+        [ValidateRange(0,1600)]
+        [int]$MaxSize
     )
-    
-    begin {
-        # declare vars
-    }
-    
+
     process {
         # check proposed config
+        $cmd = "wevtutil sl $sub"
 
-        # overwrite old subscription
-    }
-    
-    end {
+        if ($PSBoundParameters['Retention']) { $cmd += " /rt:$Retention"}
+        if ($PSBoundParameters['AutoBackup']) { $cmd += " /ab:$AutoBackup"}
+        if ($PSBoundParameters['MaxSize']) { 
+            $size = $MaxSize * 1e+6
+            $cmd += " /ms:$size"
+        }
+
+        # if no wec server prompted, localhost
+        if ($PSBoundParameters.WECServer) {
+            $res += Invoke-Command -ComputerName $WECServer -ScriptBlock { Invoke-Expression $args[0] } -ArgumentList $cmd
+        } else {
+            $res += Invoke-Expression $cmd
+        }
     }
 }
