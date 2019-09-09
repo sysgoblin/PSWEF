5de7756fac53de41cc671357523400bf68e9abb0
Bayliss <cbaylissmk2@gmail.com>
2019-09-09 07:54:54 +0100

    changed parsing from split lines to regex

--- public/Get-WEFRuntimeStatus.ps1
+++ public/Get-WEFRuntimeStatus.ps1
@@ -10,49 +10,53 @@ function Get-WEFRuntimeStatus {
         [switch]$SourceStatus
     )
     
-    begin {
-        # declare vars
+    $cmd = "wecutil gr $Subscription"
+    # get subscription runtime status
+    if ($PSBoundParameters.WECServer) {
+        $raw = Invoke-Command -ComputerName $WECServer -ScriptBlock { Invoke-Expression $args[0] } -ArgumentList $cmd
+    } else {
+        $raw = Invoke-Expression $cmd
     }
-    
-    process {
-        $cmd = "wecutil gr $Subscription"
-        # get subscription runtime status
-        if ($PSBoundParameters.WECServer) {
-            $res = Invoke-Command -ComputerName $WECServer -ScriptBlock { Invoke-Expression $args[0] } -ArgumentList $cmd
-        } else {
-            $res = Invoke-Expression $cmd
+
+    $res = ($raw -join "`n")
+
+    $pattern = '(?<=\n)\t{2}(?!\t)(.+)\n\t{3}RunTimeStatus: (.+)\n\t{3}LastError: (.+)(?:\n\t{3}ErrorMessage: ((?:\n|.)+?)\n\t{3}ErrorTime: (.+)\n\t{3}NextRetryTime: (.+))?(?:\n\t{3}LastHeartbeatTime: (.+)|$)'
+    $props = @{
+        Property =  @{Name='Name';Expression={$arr[0]}},
+                    @{Name='RunTimeStatus';Expression={$arr[1]}},
+                    @{Name='LastError';Expression={[int]$arr[2]}},
+                    @{Name='ErrorMessage';Expression={
+                        $msg = ([xml]$arr[3]).WSManFault.Message
+                        if($msg -is [string]) {$msg}
+                        else {$msg.ProviderFault.ProviderError.'#text'}
+                    }},
+                    @{Name='ErrorTime';Expression={[datetime]$arr[4]}},
+                    @{Name='NextRetryTime';Expression={[datetime]$arr[5]}},
+                    @{Name='LastHeartbeatTime';Expression={[datetime]$arr[6]}}
+    }
+
+
+    $sources = [regex]::Matches($res,$pattern,'Multiline') | % {
+        $arr = $_.Groups | select -skip 1 -exp value 
+        5 | select @props
+    }
+
+    if ($PSBoundParameters.SourceStatus){
+        $sources
+    } else {
+        $subPattern = 'Subscription: (.+)\n\t{1}RunTimeStatus: (.+)\n\t{1}LastError: (.+)$'
+        $props = @{
+            Property =  @{Name='Subscription';Expression={$arr[0]}},
+                        @{Name='RunTimeStatus';Expression={$arr[1]}},
+                        @{Name='LastError';Expression={[int]$arr[2]}},
+                        @{Name='EventSources';Expression={$sources.count}},
+                        @{Name='Active';Expression={$sources.Where({$_.RunTimeStatus -eq 'Active'}).Count}},
+                        @{Name='Inactive';Expression={$sources.Where({$_.RunTimeStatus -eq 'Inactive'}).Count}}
         }
 
-        $sourcesRaw = $res[5..$res.length]
-        $sources = @()
-        for ($i=0; $i -lt $sourcesRaw.length; $i+=4) {
-            $sources += ,($sourcesRaw[$i..($i+3)])
+        [regex]::Matches($res,$subPattern,'Multiline') | % {
+            $arr = $_.Groups | select -skip 1 -exp value 
+            5 | select @props
         }
-
-        if ($PSBoundParameters.SourceStatus){
-            $out = foreach ($s in $sources) {
-                [PSCustomObject]@{
-                    Source          = $s[0].Trim()
-                    RunTimeStatus   = ($s[1] -replace 'RunTimeStatus: ').Trim()
-                    LastError       = ($s[2] -replace 'LastError: ').Trim()
-                    LastHeartbeat   = ($s[3] -replace 'LastHeartbeatTime: ').Trim()
-                }
-            }
-        } else {
-            $out = [PSCustomObject]@{
-                Subscription    = ($res[1] -replace 'Subscription: ').Trim()
-                RunTimeStatus   = ($res[2] -replace 'RunTimeStatus: ').Trim()
-                LastError       = ($res[3] -replace 'LastError: ').Trim()
-                EventSources    = $sources.Count
-                Active          = ($sources.ForEach({$_[1]}).Where({$_ -match '\sActive'})).Count
-                Inactive        = ($sources.ForEach({$_[1]}).Where({$_ -match '\sInactive'})).Count
-            }
-        }
-        
-    }
-    
-    end {
-        # return data
-        return $out
     }
 }
