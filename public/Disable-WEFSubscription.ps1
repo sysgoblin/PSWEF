eb16750030e7ba592d689447eabd8997d83bfdcb
sysgoblin <cbaylissmk2@gmail.com>
2019-10-23 20:34:03 +0100

    changed param wecserver > server and added help

--- public/Disable-WEFSubscription.ps1
+++ public/Disable-WEFSubscription.ps1
@@ -1,23 +1,36 @@
 function Disable-WEFSubscription {
+<#
+.SYNOPSIS
+Disable specified Windows Event Forwarding subscription.
+
+.DESCRIPTION
+Disable specified Windows Event Forwarding subscription.
+
+.PARAMETER Server
+The remote Windows Event Collector server
+
+.PARAMETER Subscription
+The subscription to disable
+
+.EXAMPLE
+Disable-WEFSubscription -Server WECSVR01 -Subscription Example-Subscription
+Disables the subscription "Example-Subscription" on the Windows Event Collector server WECSVR01
+#>
+
     [CmdletBinding()]
     param (
-        [string]$WECServer,
+        [Parameter(Mandatory = $false)]
+        [string]$Server,
+
+        [Parameter(Mandatory = $true)]
         [string]$Subscription
     )
-    
-    begin {
-    }
-    
-    process {
-        $cmd = "wecutil ss $Subscription /e:false"
 
-        if ($PSBoundParameters.WECServer) {
-            Invoke-Command -ComputerName $WECServer -ScriptBlock { Invoke-Expression $args[0] } -ArgumentList $cmd
-        } else {
-            Invoke-Expression $cmd
-        }
-    }
-    
-    end {
+    $cmd = "wecutil ss $Subscription /e:false"
+
+    if ($PSBoundParameters.Server) {
+        Invoke-Command -ComputerName $Server -ScriptBlock { Invoke-Expression $args[0] } -ArgumentList $cmd
+    } else {
+        Invoke-Expression $cmd
     }
 }
--- public/Enable-WEFSubscription.ps1
+++ public/Enable-WEFSubscription.ps1
@@ -1,23 +1,36 @@
 function Enable-WEFSubscription {
+<#
+.SYNOPSIS
+Enable specified Windows Event Forwarding subscription.
+
+.DESCRIPTION
+Enable specified Windows Event Forwarding subscription.
+
+.PARAMETER Server
+The remote Windows Event Collector server
+
+.PARAMETER Subscription
+The subscription to Enable
+
+.EXAMPLE
+Enable-WEFSubscription -Server WECSVR01 -Subscription Example-Subscription
+Enables the subscription "Example-Subscription" on the Windows Event Collector server WECSVR01
+#>
+
     [CmdletBinding()]
     param (
-        [string]$WECServer,
+        [Parameter(Mandatory = $false)]
+        [string]$Server,
+
+        [Parameter(Mandatory = $true)]
         [string]$Subscription
     )
-    
-    begin {
-    }
-    
-    process {
-        $cmd = "wecutil ss $Subscription /e:true"
 
-        if ($PSBoundParameters.WECServer) {
-            Invoke-Command -ComputerName $WECServer -ScriptBlock { Invoke-Expression $args[0] } -ArgumentList $cmd
-        } else {
-            Invoke-Expression $cmd
-        }
-    }
-    
-    end {
+    $cmd = "wecutil ss $Subscription /e:true"
+
+    if ($PSBoundParameters.Server) {
+        Invoke-Command -ComputerName $Server -ScriptBlock { Invoke-Expression $args[0] } -ArgumentList $cmd
+    } else {
+        Invoke-Expression $cmd
     }
 }
